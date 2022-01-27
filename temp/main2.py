f088b9fcb217ce4329ee78a27511c895b8554cf9
mpniewskii <93130304+mpniewskii@users.noreply.github.com>
2022-01-27 20:10:58 +0100

    Add files via upload

---
+++ temp/main2.py
@@ -0,0 +1,8 @@
+def five_times(text):
+  ret = ''
+  for i in range(5):
+    ret += text
+  return ret
+
+def what_is(sth):
+  return 'garbage'
---
+++ test/test_main.py
@@ -0,0 +1,11 @@
+import main
+
+
+def test_factorial():
+    assert main.factorial(5) == 120
+    assert main.factorial(4) == 24
+
+
+def test_sum():
+    assert main.sum(1, 1) == 2
+    assert main.sum(1, 0) == 1
